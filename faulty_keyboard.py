9b8b0f9d1519867ec0b47494584b532d073f22c4
S Anas Malik <20206@fileserver1.mepcoeng.ac.in>
2026-03-23 15:23:51 +0530

    Add LeetCode solutions

---
+++ faulty_keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        res = ""
+        for c in s:
+            if c != 'i':
+                res += c
+            else:
+                res = res[::-1]
+        return res             
+        
